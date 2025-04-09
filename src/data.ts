70b5b3c5cbd18aa676951a434a157f04863cb236
Michael Lei <michaelleie0ca@gmail.com>
2025-04-09 15:46:08 -0700

    Add data file for local data

---
+++ src/data.ts
@@ -0,0 +1,71 @@
+const cities = [
+  {
+    name: "Vancouver",
+    country: "Canada",
+    description:
+      "A bustling west coast seaport in British Columbia, Vancouver is among Canada's densest, most ethnically diverse cities, surrounded by mountains and natural beauty.",
+    image: "https://images.unsplash.com/photo-1560813962-ff3d8fcf59ba",
+  },
+  {
+    name: "Toronto",
+    country: "Canada",
+    description:
+      "Canada's largest city and a multicultural hub for arts, culture, finance, and technology.",
+    image: "https://images.unsplash.com/photo-1517090504586-fde19ea6066f",
+  },
+  {
+    name: "New York City",
+    country: "United States",
+    description:
+      "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean, and is known for its iconic skyline, arts scene, and cultural diversity.",
+    image: "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9",
+  },
+  {
+    name: "San Francisco",
+    country: "United States",
+    description:
+      "San Francisco, a hilly city on the tip of a peninsula surrounded by the Pacific Ocean and San Francisco Bay, is known for its iconic Golden Gate Bridge, cable cars, and colorful Victorian houses.",
+    image: "https://images.unsplash.com/photo-1501594907352-04cda38ebc29",
+  },
+  {
+    name: "Seattle",
+    country: "United States",
+    description:
+      "Seattle, a city on Puget Sound in the Pacific Northwest, is surrounded by water, mountains and evergreen forests, and home to thousands of acres of parkland.",
+    image: "https://images.unsplash.com/photo-1502175353174-a7a70e73b362",
+  },
+  {
+    name: "Los Angeles",
+    country: "United States",
+    description:
+      "Los Angeles is a sprawling Southern California city and the center of the nation's film and television industry, with studios such as Paramount Pictures, Universal and Warner Brothers.",
+    image: "https://images.unsplash.com/photo-1580655653885-65763b2597d0",
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    description:
+      "Tokyo, Japan's busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples.",
+    image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf",
+  },
+  {
+    name: "Seoul",
+    country: "South Korea",
+    description:
+      "Seoul, the capital of South Korea, is a huge metropolis where modern skyscrapers, high-tech subways and pop culture meet Buddhist temples, palaces and street markets.",
+    image: "https://images.unsplash.com/photo-1682648353194-85d274114ef0",
+  },
+];
+
+const categories = [
+  "museum",
+  "park",
+  "landmark",
+  "historical",
+  "nature",
+  "shopping",
+  "entertainment",
+  "religious",
+  "beach",
+  "budget_friendly",
+];
