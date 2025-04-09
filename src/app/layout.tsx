0c5fdb7eae63d6688ef88bc160c6aa3663cdaa5e
Michael Lei <michaelleie0ca@gmail.com>
2025-04-09 16:19:29 -0700

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kalam } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const kalam = Kalam({
+  weight: ["300", "400", "700"],
   subsets: ["latin"],
 });
 
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh`}
-      >
+      <body className={cn(kalam.className, `antialiased min-h-dvh`)}>
         <Header />
         <main className="">{children}</main>
       </body>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import Link from "next/link";
 import UserProfile from "./auth/user-profile";
 import { Login } from "./auth/login";
+import { Compass } from "lucide-react";
 
 export default async function Header() {
   const session = await auth.api.getSession({
@@ -10,7 +11,11 @@ export default async function Header() {
   });
   return (
     <header className="container p-4 mx-auto flex justify-between">
-      <Link href="/" className="text-2xl font-bold tracking-tighter">
+      <Link
+        href="/"
+        className="text-2xl font-bold tracking-tighter flex gap-2 items-center"
+      >
+        <Compass className="size-6" />
         Next Voyage
       </Link>
       {session?.user ? <UserProfile user={session.user} /> : <Login />}
