b1a9102ac68ae8208ba629d9ae28fca310b0a0ea
Michael Lei <michaelleie0ca@gmail.com>
2025-04-10 23:07:58 -0700

    Add is selecting variable to detect if were in selecting mode

--- src/app/city/[id]/components/city-attractions.tsx
+++ src/app/city/[id]/components/city-attractions.tsx
@@ -2,16 +2,17 @@
 
 import { useState } from "react";
 
-import Link from "next/link";
 import AttractionCard from "@/components/attraction-card";
 import { Button } from "@/components/ui/button";
 import type { Attraction } from "@/data";
+import { X } from "lucide-react";
 
 export default function CityAttractions({
   cityAttractions,
 }: {
   cityAttractions: Attraction[];
 }) {
+  const [isSelecting, setIsSelecting] = useState(false);
   const [selectedAttractions, setSelectedAttractions] = useState<Attraction[]>(
     [],
   );
@@ -23,30 +24,42 @@ export default function CityAttractions({
           <AttractionCard
             key={attraction.id}
             attraction={attraction}
+            isSelecting={isSelecting}
             isSelected={selectedAttractions.some((a) => a.id === attraction.id)}
             setSelectedAttractions={setSelectedAttractions}
           />
         ))}
       </div>
-
-      {!!selectedAttractions.length && (
-        <div className="flex justify-center">
-          <div className="fixed bottom-5">
-            <span className="relative top-3 left-2 ml-auto flex size-fit">
-              <span className="text-primary-foreground relative inline-flex size-5 items-center justify-center rounded-full bg-red-500 text-center text-xs font-medium">
-                {selectedAttractions.length}
-              </span>
-            </span>
-            <Button
-              asChild
-              variant="secondary"
-              className="rounded-xl border-none px-4 py-6 text-lg font-medium"
-            >
-              <Link href={""}>Plan Itinerary</Link>
+      <div className="flex justify-center">
+        <div className="fixed bottom-5 flex items-center gap-2">
+          {isSelecting ? (
+            <>
+              <Button
+                className="rounded-full"
+                size="icon"
+                onClick={() => {
+                  setSelectedAttractions([]);
+                  setIsSelecting(false);
+                }}
+              >
+                <X className="size-4" />
+              </Button>
+              <div className="relative">
+                <Button size="lg" disabled={!selectedAttractions.length}>
+                  Generate Itinerary
+                </Button>
+                <span className="text-primary-foreground absolute -top-1 -right-1 inline-flex size-5 items-center justify-center rounded-full bg-red-500 text-center text-xs font-medium">
+                  {selectedAttractions.length}
+                </span>
+              </div>
+            </>
+          ) : (
+            <Button size="lg" onClick={() => setIsSelecting(true)}>
+              Create Itinerary
             </Button>
-          </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 }
--- src/components/attraction-card.tsx
+++ src/components/attraction-card.tsx
@@ -1,21 +1,21 @@
 "use client";
 import React, { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
-import { ExternalLink, MapPin } from "lucide-react";
 import { Attraction, categories } from "../data";
 import { Tape } from "./tape";
-import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
 import { cn } from "@/lib/utils";
 
 interface AttractionCardProps {
   attraction: Attraction;
+  isSelecting: boolean;
   isSelected: boolean;
   setSelectedAttractions: Dispatch<SetStateAction<Attraction[]>>;
 }
 
 const AttractionCard: React.FC<AttractionCardProps> = ({
   attraction,
+  isSelecting,
   isSelected,
   setSelectedAttractions,
 }) => {
@@ -26,16 +26,22 @@ const AttractionCard: React.FC<AttractionCardProps> = ({
   return (
     <div
       className={cn(
-        "relative mx-auto flex max-w-md flex-col bg-white p-3 shadow-md transition duration-300 ease-in-out hover:-translate-y-0.5 hover:scale-105 hover:cursor-pointer",
-        { "border-blue-400": isSelected },
+        "relative mx-auto flex max-w-md flex-col border-4 border-transparent bg-white p-3 shadow-md",
+        { "border-black": isSelected && isSelecting },
+        {
+          "transition duration-300 ease-in-out hover:-translate-y-0.5 hover:scale-105 hover:cursor-pointer":
+            isSelecting,
+        },
       )}
       onClick={() => {
-        setSelectedAttractions((prev) => {
-          if (prev.some((a) => a.id === attraction.id)) {
-            return prev.filter((a) => a.id !== attraction.id);
-          }
-          return [...prev, attraction];
-        });
+        if (isSelecting) {
+          setSelectedAttractions((prev) => {
+            if (prev.some((a) => a.id === attraction.id)) {
+              return prev.filter((a) => a.id !== attraction.id);
+            }
+            return [...prev, attraction];
+          });
+        }
       }}
     >
       <div className="relative">
@@ -70,25 +76,9 @@ const AttractionCard: React.FC<AttractionCardProps> = ({
           ))}
         </div>
 
-        <p className="font-handwritten mb-3 line-clamp-3 text-sm">
+        <p className="font-handwritten line-clamp-3 text-sm">
           {attraction.description}
         </p>
-        <div className="font-handwritten mb-2 flex items-start gap-1 text-xs text-gray-600">
-          <div className="flex h-[1lh] items-center">
-            <MapPin className="size-3" />
-          </div>
-          <p className="pt-0.5">{attraction.address}</p>
-        </div>
-        <Button size="lg" asChild className="text-md mt-auto flex items-center">
-          <a
-            href={attraction.googleMapUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-handwritten"
-          >
-            <span className="mt-1">View on Google Maps</span> <ExternalLink />
-          </a>
-        </Button>
       </div>
     </div>
   );
